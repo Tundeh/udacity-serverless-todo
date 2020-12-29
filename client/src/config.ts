d29cb68cc66944a539ac3bcf8cbf7d60570848fd
Tundeh <jamiu4freedom@gmail.com>
2020-12-29 06:08:33 +0100

    change apiId, domain and clientId

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'kvz6tqunm9'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-vbycqvu7.us.auth0.com',            // Auth0 domain
+  clientId: 'oKB0WzmpW5KBOxChvFz33Uh8jiR2AAWt',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
